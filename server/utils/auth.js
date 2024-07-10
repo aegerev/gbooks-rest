cd338681b79bdb432fe6ea7de2a262749a089ce8
alex <alexandersegerev@gmail.com>
2024-07-10 11:53:01 -0400

    Auth.js Complete

---
+++ server/utils/auth.js
@@ -0,0 +1,34 @@
+import jwt from 'jsonwebtoken'
+
+const secretPhrase = 'arielisthebest'
+const expireTime = '4h'
+
+module.exports = {
+    authMiddleware: function (req, res, next) {
+        let token = req.query.token || req.headers.authorization;
+
+        if(req.headers.authorization){
+            token = token.split(' ').pop().trim();
+        }
+
+        if(!token) {
+            return res.status(400).json({message: 'No token found.'});
+        }
+
+        try {
+            const {data} = jwt.verify(token, secretPhrase, {maxAge: expireTime});
+            req.user = data;
+        } catch {
+            console.log('Invalid Token!!');
+            return res.status(400).json({message: 'Invalid Token!!'});
+        }
+
+        next();
+    },
+
+    signToken: function ({username, email, _id}) {
+        const payload = {username, email, _id};
+
+        return jwt.sign({data: payload}, secretPhrase, {expiresIn: expireTime});
+    },
+};
