b6075705c06cd95f29fa370bada2f33ca15627df
alex <alexandersegerev@gmail.com>
2024-07-11 14:07:25 -0400

    custom method to compare and validate password for logging in

--- server/models/User.js
+++ server/models/User.js
@@ -40,4 +40,6 @@ userSchema.pre('save', async function (next) {
     next();
 });
 
-
+userSchema.methods.isCorrectPassword = async function(password) {
+    return bcrypt.compare(password, this.password);
+}
