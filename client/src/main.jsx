6ab59a9db211c7af2df04ec6ecc5ef6fadc0e7e8
alex <alexandersegerev@gmail.com>
2024-07-16 10:28:58 -0400

    Need to update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import App from './App.jsx'
-import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import SearchBooks from './pages/SearchBooks'
+import SavedBooks from './pages/SavedBooks'
+
+let router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    errorElement: <h1 className='display-2'>Wrong Page - try again!</h1>,
+    children: [
+      {
+        index: true,
+        element: <SearchBooks/>
+      }, {
+        path: '/saved',
+        element: <SavedBooks/>
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ <RouterProvider router={router}/>
 )
